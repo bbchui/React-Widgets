869f2ea2104bcd7cd46f46a428557c447109839a
Brandon Chui <bbchui325@gmail.com>
2017-12-28 19:03:39 -0800

    clock finished

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+const MONTHS = ["January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+];
+
 class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {time: new Date()};
     this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    let interval = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   tick() {
@@ -16,14 +38,21 @@ class Clock extends React.Component {
     let minutes = this.state.time.getMinutes();
     let seconds = this.state.time.getSeconds();
     let ampm = hours > 11 ? "PM" : "AM"
+    let month = this.state.time.getMonth();
+    let year = this.state.time.getFullYear();
+    let day = this.state.time.getDate();
 
     hours = hours > 11 ? (hours - 12) : hours;
     hours = hours < 10 ? `0${hours}` : hours;
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     seconds = seconds < 10 ? `0${seconds}` : seconds;
+    month = MONTHS[month]
 
     return(
-      <div>{hours}:{minutes}:{seconds} {ampm}</div>
+      <div>
+        <div>{hours}:{minutes}:{seconds} {ampm}</div>
+        <div>{month} {day}, {year}</div>
+      </div>
     )
   }
 }
