fff22a4a4072f11133c3b68d7cf893072b126573
Brandon Chui <bbchui325@gmail.com>
2017-12-30 23:49:26 -0800

    temperature and description showing

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -8,19 +8,21 @@ class Weather extends React.Component {
     super(props);
     this.state = {
       temperature : '',
+      description: '',
       city : '',
       lat : '',
       lon : ''
     }
+    this.ktf = this.ktf.bind(this)
   }
 
   // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
 
   componentDidMount() {
-    // axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=37&lon=-122&APPID=863f07a3e7ed2906c37b7946eb2e69fe`)
-    // .then((res) => {
-    //    console.log(res.data);
-    // })
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=37&lon=-122&APPID=863f07a3e7ed2906c37b7946eb2e69fe`)
+    .then((res) => {
+       console.log(res.data);
+    })
 
     axios.get('http://ip-api.com/json/')
     .then((res) => {
@@ -31,15 +33,30 @@ class Weather extends React.Component {
     .then(() => {
       axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&APPID=${APIKEYS.weather}`)
       .then((res) => {
-        this.setState({ temp : res.data.main.temp })
+        this.setState({ temp : res.data.main.temp,
+                        description : res.data.weather[0].description})
       })
     })
   }
 
+  ktf(temp) {
+    let f = Math.round(((temp - 273.15) * 1.8) + 32)
+    return f
+  }
+
+  getForecast(lat, lon) {
+
+  }
 
   render() {
+    let temp = this.ktf(this.state.temp)
+    let city = this.state.city
+    let description = this.state.description
+
     return(
-      <div>Weather</div>
+      <div>Weather
+        <div>Current Temperature in {city} : {temp}°F {description}</div>
+      </div>
     )
   }
 }
